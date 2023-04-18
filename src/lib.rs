d506aa9e0bfbdb87157e62cc3af533b42ed7cff6
DrEden33773 <edwardwang33773@gmail.com>
2023-04-18 22:05:39 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,124 @@
-use std::{ffi::OsString, fs};
+use std::{env, ffi::OsString, fs};
 
 type SuperError = Box<dyn std::error::Error>;
 
-pub struct Config<'a> {
-    pub query: &'a str,
-    pub file_path: &'a str,
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub if_ignore_case: bool,
 }
 
-impl<'a> Config<'a> {
-    pub fn build(args: &'a [OsString]) -> Result<Self, SuperError> {
+impl Config {
+    pub fn build_from_args(
+        query: &OsString,
+        file_path: &OsString,
+        if_ignore_case: bool,
+    ) -> Result<Self, SuperError> {
         let (query, file_path) = (
-            &args
+            query.to_str().ok_or("Invalid `query`!")?.to_string(),
+            file_path
+                .to_str()
+                .ok_or("Invalid `file_path`!")?
+                .to_string(),
+        );
+        Ok(Config {
+            query,
+            file_path,
+            if_ignore_case,
+        })
+    }
+    pub fn build_from_iter<T>(mut args: T) -> Result<Self, SuperError>
+    where
+        T: Iterator<Item = OsString>,
+    {
+        args.next();
+        let (query, file_path) = (
+            args.next()
+                .ok_or("Can't find `query` arg!")?
+                .to_str()
+                .ok_or("Invalid `query`!")?
+                .to_string(),
+            args.next()
+                .ok_or("Can't find `file_path` arg!")?
+                .to_str()
+                .ok_or("Invalid `file_path`!")?
+                .to_string(),
+        );
+        let if_ignore_case = env::var("IGNORE_CASE").is_ok()
+            || if let Some(_) = args.next() {
+                true
+            } else {
+                false
+            };
+        Ok(Config {
+            query,
+            file_path,
+            if_ignore_case,
+        })
+    }
+    pub fn build(args: &[OsString]) -> Result<Self, SuperError> {
+        let (query, file_path) = (
+            (&args
                 .get(1)
                 .ok_or("Can't find `query` arg!")?
                 .to_str()
-                .ok_or("Invalid query!")?,
-            &args
+                .ok_or("Invalid `query`!")?
+                .to_string())
+                .to_owned(),
+            (&args
                 .get(2)
                 .ok_or("Can't find `file_path` arg!")?
                 .to_str()
-                .ok_or("Invalid file_path!")?,
+                .ok_or("Invalid `file_path`!")?
+                .to_string())
+                .to_owned(),
         );
-        Ok(Config { query, file_path })
+        let if_ignore_case = env::var("IGNORE_CASE").is_ok()
+            || if let Some(_) = &args.get(3) {
+                true
+            } else {
+                false
+            };
+        Ok(Config {
+            query,
+            file_path,
+            if_ignore_case,
+        })
+    }
+    pub fn show_all_contents(&self) -> Result<(), SuperError> {
+        let contents = fs::read_to_string(&self.file_path)?;
+        println!("With text:\n{}", contents);
+        Ok(())
+    }
+    pub fn search(&self) -> Result<Vec<String>, SuperError> {
+        let contents = fs::read_to_string(&self.file_path)?;
+        let results = contents
+            .lines()
+            .filter(|line| line.contains(&self.query))
+            .map(|line| line.to_string())
+            .collect::<Vec<_>>();
+        Ok(results)
     }
     pub fn run(&self) -> Result<(), SuperError> {
-        let contents = fs::read_to_string(self.file_path)?;
-        println!("With text:\n{}", contents);
+        fn search<'a>(query: &str, contents: &'a str, if_ignore_case: bool) -> Vec<&'a str> {
+            contents
+                .lines()
+                .filter(|line| {
+                    if if_ignore_case {
+                        line.to_lowercase().contains(query)
+                    } else {
+                        line.contains(query)
+                    }
+                })
+                .collect::<Vec<_>>()
+        }
+        println!("With text:");
+        let contents = fs::read_to_string(&self.file_path)?;
+        search(&self.query, &contents, self.if_ignore_case)
+            .into_iter()
+            .for_each(|line| {
+                println!("{line}");
+            });
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,53 @@
 use mini_grep::Config;
-use std::{env, ffi::OsString, process};
+use std::{env, process};
 
 type SuperError = Box<dyn std::error::Error>;
 
 pub fn main() -> Result<(), SuperError> {
-    let args: Vec<OsString> = env::args_os().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    println!();
+    let config = Config::build_from_iter(env::args_os()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(-1);
     });
-    println!("Searching for: {}", config.query);
-    println!("In file `{}`", config.file_path);
+    println!("Searching for `{}`", &config.query);
+    println!("In file `{}`", &config.file_path);
     if let Err(err) = config.run() {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(0);
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod preludes {
+    use std::{
+        fs::{self, File},
+        io::Write,
+    };
+
+    #[test]
+    fn create_dir_and_file() -> std::io::Result<()> {
+        let contents = "
+I'm nobody! Who are you?
+我啥也不是，你呢？
+Are you nobody, too?
+牛逼如你也是无名之辈吗？
+Then there's a pair of us - don't tell!
+那我们就是天生一对，嘘！别说话！
+They'd banish us, you know.
+你知道，我们不属于这里。
+How dreary to be somebody!
+因为这里属于没劲的大人物！
+How public, like a frog
+他们就像青蛙一样呱噪，
+To tell your name the livelong day
+成天将自己的大名
+To an admiring bog!
+传遍整个无聊的沼泽！
+";
+        fs::create_dir_all("test_files")?;
+        let mut file = File::create("test_files/poem.txt")?;
+        file.write_fmt(format_args!("{}", contents))?;
+        Ok(())
+    }
 }
